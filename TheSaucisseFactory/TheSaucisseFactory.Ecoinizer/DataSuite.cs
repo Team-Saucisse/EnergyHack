a68bf271cefeffa3bb69cf98718f55b0f0b11c2c
MarcusGambit <yacine.chahrour@gmail.com>
2014-02-01 16:16:11 +0100

    Calcul des suites de valeurs par appart / mesure / semaine

--- TheSaucisseFactory/TheSaucisseFactory.Ecoinizer/DataSuite.cs
+++ TheSaucisseFactory/TheSaucisseFactory.Ecoinizer/DataSuite.cs
@@ -5,7 +5,46 @@ using System.Text;
 
 namespace TheSaucisseFactory.Ecoinizer
 {
-    class DataSuite
+    class DataSuite : List<Mesure>
     {
+        public Appartement Appartement
+        {
+            get;
+            private set;
+        }
+
+        public DateTime Date
+        {
+            get;
+            private set;
+        }
+
+        public string Type
+        {
+            get;
+            private set;
+        }
+
+        public DataSuite(MesureCollection p_mesures, Appartement p_appartement, DateTime p_date, string p_type)
+        {
+            if (p_mesures == null)
+            {
+                throw new ArgumentNullException("p_gains");
+            }
+
+            if (p_appartement == null)
+            {
+                throw new ArgumentNullException("p_appartement");
+            }
+
+            Appartement = p_appartement;
+            Date = p_date;
+            Type = p_type;
+            AddRange(p_mesures.Where(
+                g => g.AppartementId == p_appartement.Id
+                    && DateTime.Compare(g.Date, p_date) <= 0
+                    && (p_date - g.Date).TotalDays < 7
+                    && g.Type == p_type));
+        }
     }
 }
--- TheSaucisseFactory/TheSaucisseFactory.Ecoinizer/MainWindow.xaml.cs
+++ TheSaucisseFactory/TheSaucisseFactory.Ecoinizer/MainWindow.xaml.cs
@@ -16,12 +16,6 @@ namespace TheSaucisseFactory.Ecoinizer
 		public MainWindow()
 		{
 			InitializeComponent();
-
-			ThreadStart ts = new ThreadStart(RunImport);
-			Thread thread = new Thread(ts);
-			thread.Start();
-			
-			//TestCamel();
 		}
 
 		private void RunImport()
@@ -30,47 +24,14 @@ namespace TheSaucisseFactory.Ecoinizer
 			import.Run();
 		}
 
-		private void TestCamel()
-		{
-			CamelRequestResource l_req = new CamelRequestResource("2.0", "energy");
-			l_req.id = "test";
-			l_req.@params = new Params[1];
-			l_req.@params[0] = new Params();
-			l_req.@params[0].begin = DateTimeHelper.ConvertDateTimeToUnixTimeStamp(new DateTime(2013, 7, 1));
-			l_req.@params[0].end = DateTimeHelper.ConvertDateTimeToUnixTimeStamp(new DateTime(2013, 8, 2));
-			l_req.@params[0].ctx = "GLOBAL";
-			l_req.@params[0].gran = "DAY";
-			l_req.@params[0].ind = 0;
-			l_req.@params[0].lots = new string[1];
-			l_req.@params[0].lots[0] = "B101";
-			l_req.@params[0].build = "BatimentB";
-			l_req.@params[0].prog = "CERGY01";
-			l_req.@params[0].types = new string[1];
-			l_req.@params[0].types[0] = "ELEC";
-
-			if (l_req.IsValid())
-			{
-				JavaScriptSerializer l_javaScriptSerializer = new JavaScriptSerializer();
-				string l_json = l_javaScriptSerializer.Serialize(l_req);
-
-				try
-				{
-					CamelResponseResource l_res = CamelService.Post("https://camel.steria.fr:4043/CamelWebService/energy", l_json);
-				}
-				catch (CamelException ex)
-				{
-					Log.Text += ex.Message + ex.CamelMessage;
-				}
-			}
-		}
-
         /// <summary>
         /// Synchronisation Camel
         /// </summary>
         private void OnSynchronizeClick(object sender, RoutedEventArgs e)
         {
-            ImportMecanism import = new ImportMecanism(this);
-            import.Run();
+            ThreadStart ts = new ThreadStart(RunImport);
+            Thread thread = new Thread(ts);
+            thread.Start();
         }
 
         /// <summary>
@@ -78,11 +39,8 @@ namespace TheSaucisseFactory.Ecoinizer
         /// </summary>
         private void OnProcessClick(object sender, RoutedEventArgs e)
         {
-            Commerce l_newCommerce = new Commerce();
-            l_newCommerce.Nom = "Yacine";
-            l_newCommerce.Url = "http://bidon.com";
-            l_newCommerce.Save();
-                
+            Ecoinizer.EnergyCoinEngine l_engine = new EnergyCoinEngine();
+            l_engine.Save();
         }
 	}
 }
